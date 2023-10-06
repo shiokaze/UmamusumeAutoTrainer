db08af6fbeda88e864096b7580f42bf26ce4c063
shiokazexf <2240373075@qq.com>
2023-10-06 18:21:59 +0800

    fix: 修复属性值计算问题

--- module/umamusume/script/cultivate_task/event/manifest.py
+++ module/umamusume/script/cultivate_task/event/manifest.py
@@ -10,23 +10,24 @@ log = logger.get_logger(__name__)
 event_map: dict[str, Union[callable, int]] = {
     "安心～针灸师，登☆场": 5,
     "新年的抱负": scenario_event_1,
-    "新年参拜": scenario_event_2
+    "新年参拜": scenario_event_2,
+    "新年祈福": scenario_event_2
 }
 
 event_name_list: list[str] = [*event_map]
 
 
 def get_event_choice(ctx: UmamusumeContext, event_name: str) -> int:
-    event_name = find_similar_text(event_name, event_name_list, 0.8)
-    if event_name != "":
-        if event_name in event_map:
-            opt = event_map[event_name]
+    event_name_normalized = find_similar_text(event_name, event_name_list, 0.8)
+    if event_name_normalized != "":
+        if event_name_normalized in event_map:
+            opt = event_map[event_name_normalized]
             if type(opt) is int:
                 return opt
-            if type(opt) is callable:
+            if callable(opt):
                 return opt(ctx)
             else:
-                log.warning("事件[%s]未提供处理逻辑", event_name)
+                log.warning("事件[%s]未提供处理逻辑", event_name_normalized)
                 return 1
     log.debug("未知事件[%s]，使用默认选项1", event_name)
     return 1
